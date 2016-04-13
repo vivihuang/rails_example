3172ae34ef815ea904f01036637a820158c6c8d3
vivihuang <rwhuang@thoughtworks.com>
2016-04-13 15:49:39 +0800

    add basic authentication for the website

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,6 @@
 class ArticlesController < ApplicationController
+  http_basic_authenticate_with name: "root", password: "root", except: [:index, :show]
+
   def index
     @articles = Article.all
   end
