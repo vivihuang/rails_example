9d623713bcc364a7336fbb0fa6756bfdac649c3a
vivihuang <rwhuang@thoughtworks.com>
2016-04-14 13:53:15 +0800

    render to public/index.html file via rails

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,8 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  # root 'home#index'
+  match '*all', to: 'application#index', via: [:get]
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
