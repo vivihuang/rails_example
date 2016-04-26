6b1ce25788e1a203aa2aec32b145917a0a0df810
vivihuang <rwhuang@thoughtworks.com>
2016-04-26 09:54:25 +0800

    generate session controller

---
+++ app/controllers/api/v1/sessions_controller.rb
@@ -0,0 +1,14 @@
+class Api::V1::SessionsController < ApplicationController
+  include Api::V1::SessionsHelper
+  skip_before_action :authenticate_user_from_token!, only: [:create]
+
+  def create
+    authorized_user = User.authenticate(params[:username_or_email], params[:password])
+    if authorized_user
+      log_in authorized_user
+      render json: {:user => @current_user}
+    else
+      render json: {:error => 'Invalid login attempt!'}, :status => 401
+    end
+  end
+end
